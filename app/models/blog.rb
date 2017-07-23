5868321fe0add210ad6ab9df5ee87d6647e57d1c
amemiya <samemiya@nttpc.co.jp>
2017-07-23 22:56:44 +0900

    “170723101”

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
-class Blog < ActiveRecord::Base
+# DIVE14_API基礎編２ 課題 で編集
+# class Blog < ActiveRecord::Base
+# end
+
+class Poem < ActiveResource::Base #ActiveRecord::Baseから変更する
+  self.site = "https://supermarche-vin-79191.herokuapp.com/"
 end
