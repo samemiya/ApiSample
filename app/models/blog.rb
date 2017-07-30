02f817c2f910323921f3b9d6f13df1c678b896a7
amemiya <samemiya@nttpc.co.jp>
2017-07-30 18:08:20 +0900

    “170730101”

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,7 @@
 # DIVE14_API基礎編２ 課題 で編集
-# class Blog < ActiveRecord::Base
-# end
-
-class Blog < ActiveResource::Base #ActiveRecord::Baseから変更する
-  self.site = "https://supermarche-vin-79191.herokuapp.com"
+class Blog < ActiveRecord::Base
 end
+
+# class Blog < ActiveResource::Base #ActiveRecord::Baseから変更する
+#   self.site = "https://supermarche-vin-79191.herokuapp.com"
+# end
--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,7 +1,7 @@
 # DIVE14_API基礎編２ 課題 で編集
-# class Poem < ActiveRecord::Base
-# end
-
-class Poem < ActiveResource::Base #ActiveRecord::Baseから変更する
-  self.site = "https://supermarche-vin-79191.herokuapp.com"
+class Poem < ActiveRecord::Base
 end
+
+# class Poem < ActiveResource::Base #ActiveRecord::Baseから変更する
+#   self.site = "https://supermarche-vin-79191.herokuapp.com"
+# end
